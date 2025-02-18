2e95e5f53978f6054a8d31aca8c58038ad5ebdd8
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-02-18 10:03:00 +0530

    Time: 272 ms (48.61%) | Memory: 156.1 MB (37.31%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void merge(vector<int>&nums, int st, int end, int mid){
+        int leftLength = mid-st+1;
+        int rightLength = end - mid;
+
+        int *leftarr = new int[leftLength];
+        int *rightarr = new int[rightLength];
+
+        int index = st;
+
+        for(int i=0; i<leftLength; i++){
+            leftarr[i] = nums[index++];
+        }
+
+        for(int i=0; i<rightLength; i++){
+            rightarr[i] = nums[index++];
+        }
+
+        int track = st;
+        int i=0;
+        int j=0;
+
+        while(i<leftLength && j<rightLength){
+            if(leftarr[i] <= rightarr[j]){
+                nums[track++] = leftarr[i++];
+            }
+            else{
+                nums[track++] = rightarr[j++];
+            }
+        }
+
+        while(i < leftLength){
+            nums[track++] = leftarr[i++];
+        }
+
+        while(j < rightLength){
+            nums[track++] = rightarr[j++];
+        }
+    }
+    void mergeSort(vector<int>&nums, int st, int end){
+        // base case
+        if(st >= end){
+            return;
+        }
+
+        int mid = st + (end - st)/2;
+
+        mergeSort(nums, st, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, st, end, mid);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int st = 0;
+        int end = nums.size()-1;
+        mergeSort(nums, st, end);
+        return nums;
+    }
+};
