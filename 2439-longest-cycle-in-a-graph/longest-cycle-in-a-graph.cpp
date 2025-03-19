2954cc940e409bc4ab2a76e9d5380372d07969f4
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-03-19 19:58:22 +0530

    Time: 40 ms (50.76%) | Memory: 109 MB (55.88%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,45 @@
+// leetcode 2360. Longest Cycle in a Graph
+
+class Solution{
+public:
+    void dfs(int node, vector<int> &edges, vector<bool> &visited, vector<bool> &dfsTracker, vector<int> &dist, int currLength, int &longestCycleLength){
+        if (node == -1) return;
+        visited[node] = true;
+        dfsTracker[node] = true;
+        dist[node] = currLength;
+
+        int nextNode = edges[node];
+
+        if (nextNode != -1){
+            if(!visited[nextNode]){
+                dfs(nextNode, edges, visited, dfsTracker, dist, currLength + 1, longestCycleLength);
+            }
+            else if(dfsTracker[nextNode]){
+                // cycle is there.
+                int cycleLength = currLength - dist[nextNode] + 1;
+                longestCycleLength = max(longestCycleLength, cycleLength);
+            }
+        }
+        dfsTracker[node] = false;
+    }
+
+    int longestCycle(vector<int> &edges){
+        int n = edges.size();
+        vector<bool> visited(n, 0);
+        vector<bool> dfsTracker(n, 0);
+        vector<int> dist(n, 0); // we will record distances
+        int longestCycleLength = -1;
+
+        for (int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, edges, visited, dfsTracker, dist, 0, longestCycleLength);
+            }
+        }
+        return longestCycleLength;
+    }
+};
+
+
+
+
+
