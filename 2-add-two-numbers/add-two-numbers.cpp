ac76563707f3621ceaf44cfb8e245d0e28f296af
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-04-13 12:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (43.16%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,55 +11,22 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-
-
-        ListNode*&temp1 = l1;
-        ListNode*&temp2 = l2;
         ListNode*ans = new ListNode(-1);
-        ListNode*mynext = ans;
+        ListNode*gonext = ans;
         int carry = 0;
-        
 
-        while(temp1 != NULL && temp2 != NULL){
-            int sum = temp1->val + temp2 -> val + carry;
+        while(l1 || l2 || carry){
+            int a = l1 ? l1->val : 0;
+            int b = l2 ? l2 -> val : 0;
+            int sum = a + b + carry;
             carry = sum/10;
             int digit = sum%10;
-            ListNode*node_to_insert = new ListNode(digit);
-            mynext -> next = node_to_insert;
-            mynext = mynext -> next;
-            temp1 = temp1 -> next;
-            temp2 = temp2 -> next;
+            ListNode* node_to_insert = new ListNode(digit);
+            gonext -> next = node_to_insert;
+            gonext = gonext -> next;
+            l1 = l1 ? l1 -> next : nullptr;
+            l2 = l2 ? l2 -> next : nullptr;
         }
-
-        while(temp1 != NULL){
-            int sum = temp1 -> val + carry;
-            carry = sum/10;
-            int digit = sum%10;
-            ListNode*node_to_insert = new ListNode(digit);
-            mynext -> next = node_to_insert;
-            mynext = mynext -> next;
-            temp1 = temp1 -> next;
-        }
-
-        while(temp2 != NULL){
-            int sum = temp2 -> val + carry;
-            carry = sum/10;
-            int digit = sum%10;
-            ListNode*node_to_insert = new ListNode(digit);
-            mynext -> next = node_to_insert;
-            mynext = mynext -> next;
-            temp2 = temp2 -> next;
-        }
-
-        while(carry){
-            ListNode*node_to_insert = new ListNode(carry%10);
-            carry /= 10;
-            mynext -> next = node_to_insert;
-            mynext = mynext -> next;
-        }
-
-        return ans -> next;
+    return ans -> next;
     }
 };
