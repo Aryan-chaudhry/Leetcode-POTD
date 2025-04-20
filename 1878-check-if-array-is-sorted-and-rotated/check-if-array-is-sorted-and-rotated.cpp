10ffff8bb24ef4b9c16db3288e0bc410ebf52a14
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-04-20 12:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (76.32%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,43 +1,12 @@
 class Solution {
 public:
-    bool isSorted(vector<int>&ans){
-        for(int i=0; i<ans.size()-1; i++){
-            if(ans[i] > ans[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    void reverse(int st, int end, vector<int>&nums){
-        while(st <= end){
-            swap(nums[st++], nums[end--]);
-        }
-    }
-
     bool check(vector<int>& nums) {
-        int position = -1;
+        int count = 0;
 
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i] > nums[i+1]){
-                position = i;
-                break;
-            }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] > nums[(i+1)%nums.size()]) count++;
         }
 
-        if(position == -1) return true;
-
-        reverse(position+1, nums.size()-1, nums);
-        reverse(0, position, nums);
-        reverse(0, nums.size()-1, nums);
-
-
-
-        if(isSorted(nums)){
-            return true;
-        }
-        return false;
+        return count > 1 ? false : true;
     }
-    // TIME COMPLEXITY  O(n)
-    // SPACE COMPLEXITY O(1)
 };
