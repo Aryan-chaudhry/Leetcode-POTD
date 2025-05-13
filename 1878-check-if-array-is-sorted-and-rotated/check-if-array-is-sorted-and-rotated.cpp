1d4d46997e5fcee2ef0983004d96b6e3cd795680
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-05-13 16:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (45.56%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count = 0;
+        // steps to solve this problem
+        // maintain the initial state from where se start the rotation
+        // check is initial state is sorted then return true else return false
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] > nums[(i+1)%nums.size()]) count++;
+        vector<int>initial;
+        int track = -1;
+        
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] > nums[i+1]){
+                track = i+1;
+                break;
+            }
+        }
+        
+        if(track == -1){
+            // array is already in sorted form
+            return true;
         }
 
-        return count > 1 ? false : true;
+        for(int i=track; i<nums.size(); i++){
+            initial.push_back(nums[i]);
+        }
+
+        for(int i=0; i<track; i++){
+            initial.push_back(nums[i]);
+        }
+
+        for(int i=0; i<initial.size()-1; i++){
+            if(initial[i] > initial[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 };
