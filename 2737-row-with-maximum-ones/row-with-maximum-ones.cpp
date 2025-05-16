440eb55bb9f7563528cfd46308b3ffa594a8cee5
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-05-16 16:34:41 +0530

    Time: 2 ms (68.17%) | Memory: 64.8 MB (34.36%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        map<int,int>mp;
+        int maxCount = -1;
+        int row = -1;
 
         for(int i=0; i<mat.size(); i++){
             int count = 0;
-            int row = i;
             for(int j=0; j<mat[i].size(); j++){
                 if(mat[i][j] == 1){
                     count++;
                 }
             }
-            mp[row] = count;
-        }
-
-        // find max count of 1 
-        int maxCount = INT_MIN;
-
-        for(auto i : mp){
-            maxCount = max(maxCount, i.second);
-        }
-
-        // find the row having maxCount
-        int row = 0;
-
-        for(auto i : mp){
-            if(i.second == maxCount){
-                row = i.first;
-                break;
+            if(count > maxCount){
+                maxCount = count;
+                row = i;
             }
         }
-
         return {row, maxCount};
     }
 };
