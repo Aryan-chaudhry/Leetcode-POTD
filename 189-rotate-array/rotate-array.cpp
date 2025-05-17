e98632da309ccfda4a888a07357876e38602586c
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-05-17 11:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (8.74%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int size = nums.size();
-        k = k%size;
+        int n = nums.size();
+        k = k%n;
+        vector<int>rotated(n);
 
-        int track = size-k;
-        vector<int>store;
-
-        // store last k elements
-        for(int i=track; i<size; i++){
-            store.push_back(nums[i]);
+        for(int i=0; i<n; i++){
+            rotated[(i+k)%n] = nums[i];
         }
 
-        // shift array by k place
-        for(int i=size-1; i>=0; i--){
-            if(i-k >= 0){
-                nums[i] = nums[i-k];
-            }
-        }
-
-        // insert element in the begning which we store
-        for(int i=0; i<store.size(); i++){
-            nums[i] = store[i];
-        }
+        nums = rotated;
     }
 };
