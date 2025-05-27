b867ff18d05078a170bcdd52fe9b9c4f94bfb383
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-05-27 12:26:33 +0530

    Time: 29 ms (45.53%) | Memory: 23.7 MB (27.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>&grid, int srcx, int srcy){
-        if(srcx < 0 || srcx >= grid.size() || srcy < 0 || srcy >= grid[0].size() || grid[srcx][srcy] == '0') return;
+    void bfs(int r, int c, vector<vector<char>>&grid, vector<vector<bool>>&visited){
+        int m = grid.size();
+        int n = grid[0].size();
 
-        // visited matlab me es node ma aa chuka hu vapis nahi aaunga
-        grid[srcx][srcy] = '0';
+        queue<pair<int,int>>q;
+        q.push({r,c});
+        visited[r][c] = true;
 
-        // go in all possible direction
-        dfs(grid, srcx, srcy+1);
-        dfs(grid, srcx, srcy-1);
-        dfs(grid, srcx+1, srcy);
-        dfs(grid, srcx-1, srcy);
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int newr = row + drow[i];
+                int newc = col + dcol[i];
+
+                if(newr >=0 && newr < m && newc >= 0 && newc < n && !visited[newr][newc] && grid[newr][newc] == '1'){
+                    q.push({newr,newc});
+                    visited[newr][newc] = true;
+                }
+            }
+        }
     }
 
     int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>>visited(m, vector<bool>(n, false));
         int count = 0;
 
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] == '1'){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    bfs(i,j, grid, visited);
                     count++;
-                    dfs(grid, i, j);
                 }
             }
         }
