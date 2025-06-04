fdc4ffed81633e31b05ec094f64ad7e36f4d8c5b
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-06-04 10:19:11 +0530

    Time: 1833 ms (5.01%) | Memory: 54.8 MB (10.88%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-  
-
-    bool solve(vector<int>&nums){
-        vector<bool>dp(nums.size()+1, false);
-
-        dp[nums.size()-1] = true;
-
-        for(int i=nums.size()-2; i>=0; i--){
-            bool ans = false;
-            for(int jump = 1; jump<=nums[i]; jump++){
-                ans = ans || dp[i+jump];
-            }
-            dp[i] = ans;
-        } 
-        return dp[0];   
-    }
-
     bool canJump(vector<int>& nums) {
-        return solve(nums);
+       vector<int>dp(nums.size()+1, false);
+
+       // analyse base in term of dp array
+       dp[nums.size()-1] = true;
+       dp[nums.size()] =false;
+
+       for(int i=nums.size()-2; i>=0; i--){
+            for(int jump = 1; jump<=nums[i]; jump++){
+                if(i+jump < nums.size() && dp[i+jump] == true){
+                    dp[i] = true;
+                }
+            }
+       }
+    return dp[0];
     }
 };
