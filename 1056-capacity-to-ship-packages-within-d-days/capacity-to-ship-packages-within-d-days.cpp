f735bbcdcc3a6e97fcb4111450ab48bbc1d73cd6
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-06-06 11:39:44 +0530

    Time: 14 ms (40.72%) | Memory: 35.1 MB (34.97%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,37 +1,36 @@
 class Solution {
-public: 
-    int solve(vector<int>&weights, int capacity){
-        int days = 1;
+public:
+    bool isPossible(vector<int>&weights, int capacity, int days){
         int load = 0;
-        
+        int no_of_day = 1;
+
         for(int i=0; i<weights.size(); i++){
             if(load + weights[i] > capacity){
-                days = days + 1;
+                no_of_day++;
                 load = weights[i];
             }
             else{
                 load += weights[i];
             }
         }
-        return days;
+        return no_of_day <= days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int st = *(max_element(weights.begin(), weights.end()));
-        int end = accumulate(weights.begin(), weights.end(), 0);
-        int minCapacity = -1;
+        int st = *max_element(weights.begin(),weights.end());
+        long long int end = accumulate(weights.begin(), weights.end(),0);
+        int ans = -1;
 
         while(st <= end){
             int mid = st + (end - st)/2;
-            
-            if(solve(weights, mid) <= days){
-                minCapacity = mid;
+
+            if(isPossible(weights, mid, days)){
+                ans = mid;
                 end = mid - 1;
             }
             else{
                 st = mid + 1;
             }
-            mid = st + (end - st)/2;
         }
-        return minCapacity;
+        return ans;
     }
 };
