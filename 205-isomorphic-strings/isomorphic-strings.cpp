abfe02404bb40b6c94155287474cc05b89f98bab
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-06-21 18:48:52 +0530

    Time: 5 ms (12.26%) | Memory: 9.5 MB (9.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length() != t.length()) return false;
-        unordered_map<char,int>mp;
+        unordered_map<char,char>mp;
         unordered_set<char>st;
 
         for(int i=0; i<s.length(); i++){
-            if(mp.find(s[i]) != mp.end()){
+            if(mp.find(s[i]) == mp.end()){
+                // nahi hai
+
+                // hum ek case bhulte hai ki hum s to k se map to karwa rahe hai
+                // par bhul ja rahe haiki t char agar map ho gaya hai to kisi or ke sath map nahi ho sakta
+                
+                if(st.find(t[i]) == st.end()){
+                    // t char kisi ke sath map nahi hai
+                    mp[s[i]] = t[i]; // map kar deya
+                    st.insert(t[i]); // map ho gaya hai to track rakho
+                }
+                else{
+                    // t char pehele se hi kisi ke sath map hai
+                    // vapis map nahi kar sakte
+                    return false;
+                }
+
+            }
+            else{
+                // hai
                 if(mp[s[i]] != t[i]){
                     return false;
                 }
-            }
-            else{
-                if(st.find(t[i]) != st.end()){
-                    return false;
-                }
-                mp[s[i]] = t[i];
-                st.insert(t[i]);
             }
         }
         return true;
