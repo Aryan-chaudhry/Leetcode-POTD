fab16fe0ca66d6d8b3364878f3ce26a52c1e9338
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-07-07 11:55:59 +0530

    Time: 6 ms (90.63%) | Memory: 11.9 MB (23.45%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -10,18 +10,17 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(!head) return false;
-        unordered_map<ListNode*,bool>mp;
-        ListNode*temp = head;
+        ListNode*rabbit = head;
+        ListNode*tortoise = head;
 
-        while(temp != 0){
-            ListNode*node = temp;
-            if(mp.find(node) != mp.end()){
-                if(mp[node] == true){
-                    return true;
-                }
+        while(rabbit != 0){
+            rabbit = rabbit -> next;
+            if(rabbit != 0){
+                rabbit = rabbit -> next;
+                tortoise = tortoise -> next;
+
+                if(rabbit == tortoise) return true;
             }
-            mp[node] = true;
-            temp = temp -> next;
         }
         return false;
     }
