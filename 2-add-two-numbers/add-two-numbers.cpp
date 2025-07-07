8466e6cc34b4f7a499c74aff68b35c5ae5ae4a2a
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-07-07 11:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (12.16%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,25 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* l1, ListNode* l2, int carry){
-        // base case
-        if(!l1 && !l2 && !carry) return nullptr ;
-        
-
-        // ek case 
-        int a = l1 ? l1 -> val : 0;
-        int b = l2 ? l2 -> val : 0;
-        int sum = a + b + carry;
-        carry = sum/10;
-        int digit = sum%10;
-
-       ListNode*ans = new ListNode(digit);
-       ans -> next = solve(l1 ? l1->next : l1, l2 ? l2 -> next :l2, carry);
-      return ans;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode*temp1 = l1;
+        ListNode*temp2 = l2;
+
+        ListNode*node = new ListNode(-1);
+        ListNode*track = node;
+
         int carry = 0;
-        return  solve(l1, l2, carry);
-       
+
+        while(temp1 != NULL || temp2 != NULL || carry){
+            int a = temp1 ? temp1 -> val : 0;
+            int b = temp2 ? temp2 -> val : 0;
+            int sum = a + b + carry;
+            
+            int digit = sum%10;
+            carry = sum/10;
+
+            ListNode*insert = new ListNode(digit);
+            track -> next = insert;
+            track = insert;
+
+            temp1 = temp1 ? temp1 -> next : nullptr;
+            temp2 = temp2 ? temp2 -> next : nullptr;
+        }
+
+        return node -> next;
     }
 };
