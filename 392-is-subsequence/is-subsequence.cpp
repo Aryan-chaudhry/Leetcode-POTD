a7d76de3df097dd3940a586473a0fdaa8f9eafeb
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-07-11 10:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (90.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool solve(string &s, int i, string &t, int j){
-        // base case
+        // base cases
         if(i >= s.length() && j >= t.length()) return true;
         if(i < s.length() && j >= t.length()) return false;
 
@@ -12,6 +12,7 @@ public:
             return solve(s, i, t, j+1);
         }
     }
+
     bool isSubsequence(string s, string t) {
         return solve(s,0,t,0);
     }
