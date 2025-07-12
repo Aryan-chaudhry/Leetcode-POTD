b6b845457b009a822250c0dd4571860afe51b838
Aryan Chaudhary <157027129+Aryan-chaudhry@users.noreply.github.com>
2025-07-12 16:21:49 +0530

    Time: 4 ms (53.27%) | Memory: 114.2 MB (97.63%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,8 +11,8 @@
 class Solution {
 public:
     int getLength(ListNode*&head){
-        ListNode*temp = head;
         int length = 0;
+        ListNode*temp = head;
 
         while(temp != 0){
             length++;
@@ -21,7 +21,12 @@ public:
         return length;
     }
 
-    ListNode* getMid(ListNode*&fast, ListNode*&slow){
+    ListNode* findMid(ListNode*&head){
+        if(!head) return 0;
+
+        ListNode*slow = head;
+        ListNode*fast = head;
+
         while(fast != 0){
             fast = fast -> next;
             if(fast != 0){
@@ -32,35 +37,45 @@ public:
         return slow;
     }
 
-    bool isPalindrome(ListNode* head) {
-        if(head -> next == 0){
-            return true;
-        }
-        int length = getLength(head);
+   
 
-        ListNode*fast = head;
-        ListNode*slow = head;
-        ListNode*mid = getMid(fast, slow);
-
-        ListNode*prev = 0;
-        ListNode*curr = head;
-
-        while(curr != mid){
-           ListNode*frwd = curr -> next;
-           curr -> next = prev;
-           prev = curr;
-           curr = frwd; 
-        }
-
-        if(length&1) curr = curr -> next; // odd
+    bool compare(ListNode*&head, ListNode*&prev, ListNode*&curr){
         
         while(prev != 0 && curr != 0){
-            if(prev -> val != curr -> val){
-                return false;
-            }
+            if(prev -> val != curr -> val) return false;
             prev = prev -> next;
             curr = curr -> next;
         }
         return true;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head -> next == 0) return true; // ek hi node hai to vo palindrome hoi
+
+        // step 1 --> find the length of linkedlist
+        int length = getLength(head);
+
+        // step 2 --> find mid Node
+        ListNode*mid = findMid(head);
+
+        // step 3 --> reverse the first half of the node
+        
+        ListNode*prev = 0;
+        ListNode*curr = head;
+        
+        while(curr != mid){
+            ListNode*frwd = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = frwd;
+        }
+
+        // step 4 --> check karo length of linkedlist and get your ans
+        // agar even hai to curr se hi compare karte chalange
+        // agar odd hai to ek node extra hai to curr ko ek node aage move kardo
+        if(length&1) curr = curr -> next; // odd length thi aage move kar gaye
+        
+        bool ans = compare(head, prev, curr);
+        return ans;
     }
 };
